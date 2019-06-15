37cfe385cc7fabcb99c53fc1bc43553af13c3e5e
Deepak Vangala <44211489+dvangala21@users.noreply.github.com>
2019-06-15 11:29:53 -0500

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,21 @@
+console.log(data); 
+
+function init() {
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
+  
+    // Use the list of sample names to populate the select options
+    d3.json("/names").then((sampleNames) => {
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  
+      // Use the first sample from the list to build the initial plots
+      const firstSample = sampleNames[0];
+      buildCharts(firstSample);
+      buildMetadata(firstSample);
+    });
+  }
